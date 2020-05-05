33f10d6e1c94ee67d7242794c07e86e53158881a
root <lily9393@gmail.com>
2020-05-05 08:38:45 +0000

    completed project

--- src/route_planner.h
+++ src/route_planner.h
@@ -2,32 +2,36 @@
 #define ROUTE_PLANNER_H
 
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
+#include <queue>
+
 #include "route_model.h"
 
-
 class RoutePlanner {
-  public:
-    RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
-    // Add public variables or methods declarations here.
-    float GetDistance() const {return distance;}
-    void AStarSearch();
+ public:
+  RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x,
+               float end_y);
+  float GetDistance() const { return distance; }
+  void AStarSearch();
 
-    // The following methods have been made public so we can test them individually.
-    void AddNeighbors(RouteModel::Node *current_node);
-    float CalculateHValue(RouteModel::Node const *node);
-    std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node *);
-    RouteModel::Node *NextNode();
+  void AddNeighbors(RouteModel::Node *current_node);
+  float CalculateHValue(RouteModel::Node const *node);
+  std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node *);
+  RouteModel::Node *NextNode();
 
-  private:
-    // Add private variables or methods declarations here.
-    std::vector<RouteModel::Node*> open_list;
-    RouteModel::Node *start_node;
-    RouteModel::Node *end_node;
+ private:
+  struct NodeGreater{
+    bool operator()(const RouteModel::Node* lhs, const RouteModel::Node* rhs){
+      return lhs->g_value + lhs->h_value > rhs->g_value + rhs->h_value;
+    }
+  };
+  std::priority_queue<RouteModel::Node*, std::vector<RouteModel::Node*>, NodeGreater> open_list;
+  RouteModel::Node *start_node;
+  RouteModel::Node *end_node;
 
-    float distance = 0.0f;
-    RouteModel &m_Model;
+  float distance = 0.0f;
+  RouteModel &m_Model;
 };
 
 #endif
